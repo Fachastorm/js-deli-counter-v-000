2693396d04dfb963a49c0a28cdcbae39458784aa
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-16 18:38:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ function currentLine(line) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
   return `The line is currently: ${numbersAndNames.join(', ')}`
+};
+
+function nowServing(line) { 
+  if (!line.length) {
+    return "Theere is nobody waiting to be served!"
+  }
+  return `Currently serving ${line.shift()}`
 }
-
-
 
 function takeANumber(line, name) {
   line.push(name)
