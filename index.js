47da67c381b6e6d421d8e8bf75490d0b96e3a6ac
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2018-02-16 18:36:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently is empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++)
+}
+
+
+
+function takeANumber(line, name) { 
+  line.push(name)
+
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+  
+}
+
